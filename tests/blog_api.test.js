419f9bbe30426fb0c8b10d3bd381f1c7ab32231c
LiuBaoYing <92360139+LBY89@users.noreply.github.com>
2022-03-21 09:53:41 +0200

    Update blog_api.test.js

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const app = require('../app')
 const helper = require('./list_helper')
-
+const jwt = require('jsonwebtoken')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+let token
+
+beforeEach(async () => {
+
+    await User.deleteMany({})
+    await Blog.deleteMany({})
+    
+    const newUser = {
+    username: "root",
+    password: "password",
+    }
+
+    await api.post("/api/users").send(newUser)
+    await Blog.insertMany(helper.initialBlogs)
+
+    const result = await api.post("/api/login").send(newUser)
+
+    token = result.body.token
+})
 
 describe('initial state contain blogs', () => {
-    beforeEach(async () => {
-        await Blog.deleteMany({})
-        await Blog.insertMany(helper.initialBlogs)
-    })
+
       
       //4.8 all blog posts in the JSON format
     test('blogs are returned as json', async () => {
@@ -33,34 +52,43 @@ describe('initial state contain blogs', () => {
         console.log('blogs', blogs)
         expect(blogs[0].id).toBeDefined()
     })
-
+}) 
     //4.10
+describe("add blog", () => {
+    
+
     test('a valid blog can be added', async () => {
-    const newBlog = {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-    }
+      
+        const newBlog = {
+            
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+        }
+       
+        await api
+            .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const response = await api.get('/api/blogs')
 
-    const response = await api.get('/api/blogs')
+        const titles = response.body.map(r => r.title)
 
-    const titles = response.body.map(r => r.title)
-
-    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
-    expect(titles).toContain(
-        'Canonical string reduction'
-    )
+        expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+        expect(titles).toContain(
+            'Canonical string reduction'
+        )
     })
     //4.11
+})
+    
+
+describe('something missing', () => {
+
     test('if missing likes default to 0', async () => {
         const newBlog = {
             _id: "5a422b891b54a676234d17fa",
@@ -72,6 +100,7 @@ describe('initial state contain blogs', () => {
         
         await api
             .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
             .send(newBlog)
             .expect(201)
             .expect('Content-Type', /application\/json/)
@@ -86,13 +115,13 @@ describe('initial state contain blogs', () => {
     //4.12
     test('title and url missing', async () => {
         const newBlog = {
-    
             author: "Liu",
             likes: 0
         }
         
         await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token}`)
         .send(newBlog)
         .expect(400)
 
@@ -101,8 +130,52 @@ describe('initial state contain blogs', () => {
         expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
         
     })
-}) 
+    //4.13
+})
 
+describe('deletion of a blog',() => {
+    
+    test('succeeds with status code 204 if id is valid', async () => {
+    
+        const blogsAtStart = await helper.blogsInDb()
+        console.log('blogsAtStart', blogsAtStart)
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        console.log('blogsAtEnd', blogsAtEnd)
+        expect(blogsAtEnd).toHaveLength(
+            helper.initialBlogs.length - 1
+        )
+
+        const titles = blogsAtEnd.map(r => r.title)
+
+        expect(titles).not.toContain(blogToDelete.title)
+    })
+})
+
+    //4.14
+describe('update of a blog', () => {
+    test('update successful with status code 202 if id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        //const blogToUpdate = blogsAtStart[0]
+        const newBlog = {
+            likes: 66
+        }
+
+        await api
+            .put(`/api/blogs/${blogsAtStart[0].id}`)
+            .send(newBlog)
+            .expect(202)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        console.log('blogsAtEnd', blogsAtEnd)
+        expect(blogsAtEnd[0].likes).toEqual(66)
+    })
+})
 
 
 afterAll(() => {
